2bf88eb8cce06ba22b30d7e848266b1490ee7847
SamerAlsayegh <77DkTZqPKmrW#ffC>
2020-04-05 15:53:36 -0400

    fixed data upload

--- src/interfaces/User/Pages/DataList/Header.js
+++ src/interfaces/User/Pages/DataList/Header.js
@@ -6,6 +6,7 @@ import { Grid, Typography, Button } from '@material-ui/core';
 import { Formik } from 'formik';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from "@material-ui/core/DialogTitle";
+import UploadDialog from "../../../../components/Dialogs/UploadDialog";
 
 const useStyles = makeStyles(() => ({
   root: {}
@@ -14,28 +15,12 @@ const useStyles = makeStyles(() => ({
 function Header({ className, ...rest }) {
   const classes = useStyles();
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
 
     <div
       {...rest}
       className={clsx(classes.root, className)}
     >
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Add Data</DialogTitle>
-        <div>
-            <h1> OPTION NOT NEEDED</h1>
-        </div>
-      </Dialog>
       <Grid
         alignItems="flex-end"
         container
@@ -58,13 +43,14 @@ function Header({ className, ...rest }) {
           </Typography>
         </Grid>
         <Grid item>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={handleClickOpen}
-          >
-            Upload Data
-          </Button>
+          <UploadDialog/>
+          {/*<Button*/}
+          {/*  color="primary"*/}
+          {/*  variant="contained"*/}
+          {/*  onClick={handleClickOpen}*/}
+          {/*>*/}
+          {/*  Upload Data*/}
+          {/*</Button>*/}
         </Grid>
       </Grid>
     </div>
--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -3,10 +3,10 @@ const MAPTILER_ACCESS_TOKEN = '26cLttQaRCsX5ga6DFG1';
 
 
 const dev = {
-    // api: 'http://ec2-18-218-128-107.us-east-2.compute.amazonaws.com:8001/api',
-    api: 'http://localhost:8001/api',
+    api: 'http://ec2-18-218-128-107.us-east-2.compute.amazonaws.com:8001/api',
+    // api: 'http://localhost:8001/api',
     mapProvider: function mapTilerProvider(x, y, z, dpr) {
-        return `https://maps.wikimedia.org/osm-intl/${z}/${x}/${y}.png`
+        return `https://api.maptiler.com/maps/${MAP_ID}/256/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png?key=${MAPTILER_ACCESS_TOKEN}`
     }
 };
 
